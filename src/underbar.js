eb29964cd721b7af9c3afc74e9e24fdb1b6593bd
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-03-31 13:02:54 -0700

    Completed "filter"

--- src/underbar.js
+++ src/underbar.js
@@ -77,6 +77,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+    for (var i=0; i<collection.length; i++) {
+      if (test(collection[i])) {
+        results.push(collection[i]);
+      }
+    }
+    return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
