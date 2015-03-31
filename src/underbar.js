fc5cd7b0fd1dae1ba8f9d555e8cec03ac4f37727
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-03-31 13:55:38 -0700

    Completed "reject"

--- src/underbar.js
+++ src/underbar.js
@@ -90,6 +90,9 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(a) {
+      return !test(a);
+    });
   };
 
   // Produce a duplicate-free version of the array.
